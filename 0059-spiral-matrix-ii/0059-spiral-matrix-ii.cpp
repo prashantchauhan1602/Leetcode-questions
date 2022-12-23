b4bf719f0ab651c502833bdc9fcbfa94b9b9651e
Prashant Chauhan <114959380+prashantchauhan1602@users.noreply.github.com>
2022-12-23 14:40:59 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (86.14%) - LeetHub

---
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        
+        int starting_row = 0 ;
+        int ending_row = n - 1 ;
+        int starting_col = 0 ;
+        int ending_col = n - 1 ;
+        
+        vector<vector<int>> arr (n , vector<int> (n , 0));
+
+        int value = 1;
+        int total = n * n ;
+        
+        while(starting_row <= ending_row && starting_col  <= ending_col && value <= total)
+        {
+            for(int i = starting_col ; i <= ending_col && starting_row <= ending_row && starting_col  <= ending_col ; i++)
+            {
+                arr[starting_row][i] = value++ ;
+            }
+            starting_row++ ;
+            
+            for(int i = starting_row ; i <= ending_row && starting_row <= ending_row && starting_col  <= ending_col; i++)
+            {
+                arr[i][ending_col] = value++ ;
+            }
+            ending_col-- ;
+            
+            if(starting_row < ending_row && starting_col < ending_col)
+            {
+                for(int i = ending_col ; i >= starting_col ; i--)
+                {
+                    arr[ending_row][i] = value++ ;
+                }
+                ending_row-- ;
+            
+                for(int i = ending_row ; i >= starting_row ; i--)
+                {
+                    arr[i][starting_col] = value++ ;
+                }
+                starting_col++ ;
+            }
+        }
+        return arr ;
+        
+       
+    }
+};
