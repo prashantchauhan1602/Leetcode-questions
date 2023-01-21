2525327399b3531f20203f0ad67c0f8cd9f3404b
Prashant Chauhan <114959380+prashantchauhan1602@users.noreply.github.com>
2023-01-22 02:28:30 +0530

    Added solution - LeetHub

--- Diameter of a Binary Tree - GFG/diameter-of-a-binary-tree.cpp
+++ Diameter of a Binary Tree - GFG/diameter-of-a-binary-tree.cpp
@@ -92,34 +92,59 @@ struct Node
 }; */
 
 class Solution {
-    private :
-    int height(Node* root)
+    // private :
+    // int height(Node* root)
+    // {
+    //     if(root == NULL)
+    //     {
+    //         return 0 ;
+    //     }
+    //     int left = height(root -> left) ;
+    //     int right = height(root -> right) ;
+        
+    //     int ans = max(left , right) + 1 ;
+    //     return ans ;
+    // }
+  public:
+  pair<int , int> isDiameter(Node* root)
     {
         if(root == NULL)
         {
-            return 0 ;
+            pair<int , int> p = make_pair(0 , 0 ) ;
+            return p; 
         }
-        int left = height(root -> left) ;
-        int right = height(root -> right) ;
         
-        int ans = max(left , right) + 1 ;
+        pair<int , int> left = isDiameter(root -> left) ;
+        pair<int , int> right = isDiameter(root -> right) ;
+        
+        int op1 = left.first ;
+        int op2 = right.first ;
+        int op3 = left.second + right.second + 1 ;
+        
+        pair<int , int> ans ;
+        ans.first = max(op1 , max(op2 , op3)) ;
+        ans.second = max(left.second , right.second) + 1 ;
+        
         return ans ;
     }
-  public:
     // Function to return the diameter of a Binary Tree.
     int diameter(Node* root) {
-        if(root == NULL)
-        {
-            return 0 ;
-        }
         
-        int op1 = diameter(root -> left) ;
-        int op2 = diameter(root -> right) ;
-        int op3 = height(root -> left) + height(root -> right) + 1 ;
+        return isDiameter(root).first ;
+    //     if(root == NULL)
+    //     {
+    //         return 0 ;
+    //     }
         
-        int ans = max(op1 , max(op2 , op3)) ;
-        return ans ;
+    //     int op1 = diameter(root -> left) ;
+    //     int op2 = diameter(root -> right) ;
+    //     int op3 = height(root -> left) + height(root -> right) + 1 ;
+        
+    //     int ans = max(op1 , max(op2 , op3)) ;
+    //     return ans ;
     }
+    
+    
 };
 
 //{ Driver Code Starts.
