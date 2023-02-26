8ac027e6582292eac4f6e6bfa223bc1d779a6c7b
Prashant Chauhan <114959380+prashantchauhan1602@users.noreply.github.com>
2023-02-26 05:54:11 +0530

    Added solution - LeetHub

---
+++ Find all pairs with a given sum - GFG/find-all-pairs-with-a-given-sum.cpp
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    vector<pair<int,int>> allPairs(int A[], int B[], int N, int M, int X)
+    {
+        sort(A , A+N) ;
+        sort(B , B+M) ;
+        // sort(A.begin() , A.end()) ;
+        // sort(B.begin() , B.end()) ;
+        // A.sort() ;
+        
+        vector<pair<int , int>> result ;
+        pair<int , int> output ;
+        for(int i = 0 ; i < N ; i++)
+        {
+            for(int j = 0 ; j < M ; j++)
+            {
+                if(A[i] + B[j] == X)
+                {
+                    pair<int , int> output = make_pair(A[i] , B[j]);
+                    result.push_back(output) ;
+                    // output.first = A[i] ;
+                    // output.second = B[j] ;
+                }
+                if(A[i] + B[j] > X)
+                {
+                    break  ;
+                }
+            }
+            // result.push_back(output) ;
+        }
+        return result ;
+        // return pair1 ;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+	long long t;
+	cin >> t;
+	
+	while(t--){
+	    int N, M, X;
+	    cin >> N >> M >> X;
+        int A[N], B[M];
+        
+	    for(int i = 0;i<N;i++)
+	        cin >> A[i];
+	    for(int i = 0;i<M;i++)
+	        cin >> B[i];
+	        
+	   Solution ob;
+	   vector<pair<int,int>> vp = ob.allPairs(A, B, N, M, X);
+	   int sz = vp.size();
+        if(sz==0)
+        cout<<-1<<endl;
+        else{
+            for(int i=0;i<sz;i++){
+                if(i==0)
+                cout<<vp[i].first<<" "<<vp[i].second;
+                else
+                cout<<", "<<vp[i].first<<" "<<vp[i].second;
+            }
+            cout<<endl;
+        }
+	}
+	return 0;
+}
+// } Driver Code Ends
