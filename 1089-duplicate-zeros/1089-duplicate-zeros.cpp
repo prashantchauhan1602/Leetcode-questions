290d7ffc706dd3897285aa15060824db6e753f07
Prashant Chauhan <114959380+prashantchauhan1602@users.noreply.github.com>
2023-02-26 06:10:50 +0530

    Time: 5 ms (80.78%), Space: 10 MB (26.31%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.cpp
+++ 1089-duplicate-zeros/1089-duplicate-zeros.cpp
@@ -5,18 +5,12 @@ public:
         vector<int> ans ;
         for(int i = 0 ; i < size && ans.size() < size ; i++)
         {
-            
-            // if(ans.size() <= arr.size())
-            // {
                 ans.push_back(arr[i]);
                 if(arr[i] == 0 && ans.size() < size)
                 {
                     ans.push_back(0) ;
                 }
-            // }
-            // else{
-            //     return ;
-            // }
+            
         }
         arr = ans ;
     }
