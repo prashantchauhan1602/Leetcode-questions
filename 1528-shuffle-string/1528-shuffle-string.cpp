dc5e17d48b7b2a1b430ec144a07368be6e16a59d
Prashant Chauhan <114959380+prashantchauhan1602@users.noreply.github.com>
2023-09-09 19:45:18 +0530

    Time: 9 ms (36.39%), Space: 15.9 MB (12.48%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.cpp
+++ 1528-shuffle-string/1528-shuffle-string.cpp
@@ -4,13 +4,26 @@ public:
         string ans ="";
         int size = s.size();
         
+        // for(int i=0; i<size;i++){
+        //     for(int j=0; j<size; j++){
+        //         if(indices[j] == i){
+        //             ans.push_back(s[j]);
+        //         }
+        //     }
+        // }
+        
+        // string ans ="";
+        vector<pair<int,char>>pairs;
         for(int i=0; i<size;i++){
-            for(int j=0; j<size; j++){
-                if(indices[j] == i){
-                    ans.push_back(s[j]);
-                }
-            }
+            pairs.push_back(make_pair(indices[i], s[i])); 
         }
+        
+        sort(pairs.begin(),pairs.end());
+        
+        for(int i=0; i<size; i++){
+            ans.push_back(pairs[i].second);
+        }
+        
         return ans;
     }
 };
