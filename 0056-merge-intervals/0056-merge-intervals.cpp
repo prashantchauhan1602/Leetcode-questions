eacf936cf59dd9c2494eb1594999a42751ffb008
Prashant Chauhan <114959380+prashantchauhan1602@users.noreply.github.com>
2023-09-10 17:39:03 +0530

    Time: 29 ms (71.42%), Space: 19.1 MB (46.21%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -6,27 +6,33 @@ public:
         sort(intervals.begin(),intervals.end());
         
         for(int i=0; i<size; i++){
-            // int start = intervals[i][0];
-            // int end = intervals[i][1];
+            int start = intervals[i][0];
+            int end = intervals[i][1];
             
-            // for(int j=1; j<size; j++){
-            //     if(intervals[j][0] <= end){
-            //         end = max(intervals[j][1], end);
-            //     }
-            //     else{
-            //         ans.push_back({start,end});
-            //     }
-            // }
-            
-            if(ans.empty() || ans.back()[1] < intervals[i][0]){
-                ans.push_back(intervals[i]);
+            if(!ans.empty() && end <= ans.back()[1]){
+                continue;
             }
             
-            else{
-                if(ans.back()[1] >= intervals[i][0]){
-                    ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+            for(int j=i+1; j<size; j++){
+                if(intervals[j][0] <= end){
+                    end = max(intervals[j][1], end);
+                }
+                else{
+                    break;
                 }
             }
+            
+            ans.push_back({start,end});
+            
+//             if(ans.empty() || ans.back()[1] < intervals[i][0]){
+//                 ans.push_back(intervals[i]);
+//             }
+            
+//             else{
+//                 if(ans.back()[1] >= intervals[i][0]){
+//                     ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+//                 }
+//             }
         }
         return ans;
     }
