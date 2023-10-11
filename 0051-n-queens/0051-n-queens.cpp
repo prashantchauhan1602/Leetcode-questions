cb14be53050523349d74dfca4d677cc11406d4a5
Prashant Chauhan <114959380+prashantchauhan1602@users.noreply.github.com>
2023-10-11 19:52:44 +0530

    Time: 7 ms (49.86%), Space: 8.5 MB (27.95%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,72 +1,107 @@
 class Solution {
 public:
-    bool isSafe(vector<string>&board,int row,int col,int n){
+    
+    unordered_map<int,bool>rowCheck;
+    unordered_map<int,bool> upperLeftDiagonal;
+    unordered_map<int, bool> bottomLeftDiagonal;
+    
+    bool isSafe(vector<vector<string>>&board,int row,int col,int n){
         // upper diagonal...
-        int i=row;
-        int j=col;
-        while(i>=0 && j>=0){
-            if(board[i][j] == 'Q'){
-                return false;
-            }
-            i--;
-            j--;
-        }
+        // int i=row;
+        // int j=col;
+        // while(i>=0 && j>=0){
+        //     if(board[i][j] == "Q"){
+        //         return false;
+        //     }
+        //     i--;
+        //     j--;
+        // }
         
         //  left part..
-        i=row;
-        j=col;
-        while(j >= 0){
-            if(board[i][j] == 'Q'){
-                return false;
-            }
-            j--;
-        }
+        // i=row;
+        // j=col;
+        // while(j >= 0){
+        //     if(board[i][j] == "Q"){
+        //         return false;
+        //     }
+        //     j--;
+        // }
         
         // lower triangle
-        i=row;
-        j=col;
-        while(i < n && j>=0){
-            if(board[i][j] == 'Q'){
-                return false;
-            }
-            i++;
-            j--;
-        }
+//         i=row;
+//         j=col;
+//         while(i < n && j>=0){
+//             if(board[i][j] == "Q"){
+//                 return false;
+//             }
+//             i++;
+//             j--;
+//         }
         
+//         return true;
+        
+//         Second Approach...
+        
+        if(rowCheck[row] == true){
+            return false;
+        }
+        if(upperLeftDiagonal[n-1+col-row] == true){
+            return false;
+        }
+        if(bottomLeftDiagonal[row+col] == true){
+            return false;
+        }
         return true;
     }
+   
     
-    void addSol(vector<string>&board, int n, vector<vector<string>>&ans){
+   void addSol(vector<vector<string>>&board, int n, vector<vector<string>>&ans){
         vector<string>temp;
+        
         for(int i=0; i<n; i++){
-            temp.push_back(board[i]);
+            string str = "";
+            for(int j=0; j<n; j++){
+                str += board[i][j];
+            }
+            temp.push_back(str);
         }
         ans.push_back(temp);
     }
     
-    void solve(vector<string>&board, int col, vector<vector<string>>&ans, int n){
+    void solve(vector<vector<string>>&board, int col, vector<vector<string>>&ans, int n){
         if(col >= n){
             addSol(board, n, ans);
             return ;
         }
         
         for(int row=0; row < n; row++){
-            if(isSafe(board, row, col,n)){
+//             if(isSafe(board, row, col,n)){
+//                 board[row][col] = 'Q';
+                
+            
+//                 solve(board,col+1,ans,n);
+            
+//                 board[row][col] = '.';
+//             }
+            
+            if(isSafe(board,row,col,n)){
                 board[row][col] = 'Q';
-            
+                rowCheck[row] = true;
+                upperLeftDiagonal[n-1+col-row] = true;
+                bottomLeftDiagonal[row+col] =true;
+                
                 solve(board,col+1,ans,n);
-            
+                
                 board[row][col] = '.';
+                rowCheck[row] = false;
+                upperLeftDiagonal[n-1+col-row] = false;
+                bottomLeftDiagonal[row+col] =false;
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
-        string str="";
         vector<vector<string>>ans;
-        for(int i=0; i<n; i++){
-            str += '.' ;
-        }
-        vector<string> board(n,str);
+        vector<vector<string>> board(n, vector<string>(n,"."));
         int col = 0;
         solve(board, col, ans, n);
         return ans;
