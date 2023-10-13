8701361111bc32f298e9a8d70282a6bf95ac14c5
Prashant Chauhan <114959380+prashantchauhan1602@users.noreply.github.com>
2023-10-13 14:27:06 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (10.58%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.cpp
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.cpp
@@ -1,33 +1,59 @@
 class Solution {
 public:
     string sortSentence(string s) {
-        int size = s.size();
-        int count_num = 10;
-        vector<string>mapping(size);
-        string temp = "";
-        string output;
+//         int size = s.size();
+//         vector<string>mapping(size);
+//         string temp = "";
+//         string output;
         
-        for(int i=0; i<size; i++){
+//         for(int i=0; i<size; i++){
             
+//             if(isalpha(s[i])){
+//                 temp.push_back(s[i]);
+//             }
+            
+//             if(isdigit(s[i])){
+//                 int digit = s[i] - '0';
+//                 if(temp != ""){
+//                     mapping[digit] = temp;
+//                 }
+//                 temp = "";
+//             }
+//         }
+        
+//         for(int i=0; i<size; i++){
+//             if(mapping[i].size() > 0){
+//                 output +=mapping[i];
+//                 output += ' ';
+                
+//             }
+//         }
+//         output.pop_back();
+//         return output;
+        
+        string output = "";
+        string temp="";
+        int size = s.size();
+        vector<pair<int,string>>pair;
+        for(int i=0; i<size; i++){
             if(isalpha(s[i])){
                 temp.push_back(s[i]);
             }
             
             if(isdigit(s[i])){
                 int digit = s[i] - '0';
-                if(temp != ""){
-                    mapping[digit] = temp;
-                }
+                // if(temp != ""){
+                //     mapping[digit] = temp;
+                // }
+                pair.push_back({digit, temp});
                 temp = "";
             }
         }
+        sort(pair.begin(), pair.end());
         
-        for(int i=0; i<size; i++){
-            if(mapping[i].size() > 0){
-                output +=mapping[i];
-                output += ' ';
-                
-            }
+        for(auto it:pair){
+            output +=it.second;
+            output +=' ';
         }
         output.pop_back();
         return output;
