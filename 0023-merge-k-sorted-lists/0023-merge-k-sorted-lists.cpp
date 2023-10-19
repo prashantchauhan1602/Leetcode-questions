ee3d3dd4d24937a835b51a2ae03cd50c6a7dd4c1
Prashant Chauhan <114959380+prashantchauhan1602@users.noreply.github.com>
2023-10-20 02:38:17 +0530

    Time: 17 ms (65.47%), Space: 13.8 MB (43.91%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -9,57 +9,91 @@
  * };
  */
 
-
+class Compare{
+    public:
+    bool operator()(ListNode* a1, ListNode* a2){
+        return a1->val > a2->val;
+    }
+};
 class Solution {
 public:
     
-    ListNode* merge(ListNode*left, ListNode*right){
-        ListNode* dummy = new ListNode(0);
-        ListNode* curr = dummy;
+//     ListNode* merge(ListNode*left, ListNode*right){
+//         ListNode* dummy = new ListNode(0);
+//         ListNode* curr = dummy;
         
-        while(left != NULL && right != NULL){
-            if(left->val < right->val){
-                curr -> next = left;
-                left = left -> next;
-            }
-            else{
-                curr -> next = right;
-                right = right -> next;
-            }
-            curr = curr -> next;
-        }
-        while(left != NULL){
-            curr -> next = left;
-            left = left -> next;
-            curr = curr -> next;
-        }
-        while(right != NULL){
-            curr -> next = right;
-            right = right -> next;
-            curr = curr -> next;
-        }
-        return dummy->next;
-    }
-    ListNode*mergeSort(vector<ListNode*>&lists, int start, int end){
-        if(start == end){
-            return lists[start];
-        }
-        if(start+1 == end){
-            return merge(lists[start], lists[end]);
-        }
+//         while(left != NULL && right != NULL){
+//             if(left->val < right->val){
+//                 curr -> next = left;
+//                 left = left -> next;
+//             }
+//             else{
+//                 curr -> next = right;
+//                 right = right -> next;
+//             }
+//             curr = curr -> next;
+//         }
+//         while(left != NULL){
+//             curr -> next = left;
+//             left = left -> next;
+//             curr = curr -> next;
+//         }
+//         while(right != NULL){
+//             curr -> next = right;
+//             right = right -> next;
+//             curr = curr -> next;
+//         }
+//         return dummy->next;
+//     }
+//     ListNode*mergeSort(vector<ListNode*>&lists, int start, int end){
+//         if(start == end){
+//             return lists[start];
+//         }
+//         if(start+1 == end){
+//             return merge(lists[start], lists[end]);
+//         }
         
-        int mid = start + (end - start)/2;
-        ListNode* left = mergeSort(lists, start, mid);
-        ListNode* right = mergeSort(lists, mid+1, end);
+//         int mid = start + (end - start)/2;
+//         ListNode* left = mergeSort(lists, start, mid);
+//         ListNode* right = mergeSort(lists, mid+1, end);
         
-        return merge(left,right);
-    }
+//         return merge(left,right);
+//     }
     
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if(lists.empty()){
+        
+        // Solution 1: Min Heap Creation...
+        priority_queue<ListNode*, vector<ListNode*>, Compare> pq;
+        for(ListNode *head:lists){
+            while(head){
+                pq.push(head);
+                head = head -> next;
+            }
+        }
+
+        if(pq.empty()){
             return NULL;
         }
+
+        ListNode* prev = pq.top();
+        ListNode* ans = prev;
+        pq.pop();
+
+        while(!pq.empty()){
+            ListNode* temp = pq.top();
+            pq.pop();
+            prev -> next = temp;
+            prev = temp;
+        }
+        prev -> next = NULL;
+        return ans;
+    
+        // Solution 2: By Using Merge Sort...
+//         if(lists.empty()){
+//             return NULL;
+//         }
         
-        return mergeSort(lists, 0, lists.size() -1);
+//         return mergeSort(lists, 0, lists.size() -1);
+        
     }
 };
